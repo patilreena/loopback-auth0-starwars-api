31e05420865d05eb73af3ca9efa23b98c0ee5160
Reena Patil <reena.patil@outlook.com>
2017-12-09 19:29:17 +1100

    Added Auth0 jwt authentication

--- server/server.js
+++ server/server.js
@@ -2,8 +2,36 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
+var app = (module.exports = loopback());
 
-var app = module.exports = loopback();
+var authCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://starwars.au.auth0.com/.well-known/jwks.json',
+  }),
+  audience: 'http://localhost:3000/',
+  issuer: 'https://starwars.au.auth0.com/',
+  algorithms: ['RS256'],
+});
+
+app.use(authCheck);
+
+app.use('/api/films', function(req, res, next) {
+  res.json('It has valid token', req.user);
+});
+
+// catch error
+app.use(function(err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Invalid token, or no token supplied!');
+  } else {
+    res.status(401).send(err);
+  }
+});
 
 app.start = function() {
   // start the web server
@@ -24,6 +52,5 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) app.start();
 });
